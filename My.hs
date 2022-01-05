1f7a5b972b01f8f1681dc532641c312a3a4398e9
Mathieu Brujan <ken@localhost.localdomain>
2022-01-05 09:31:27 -0500

    First 24 tasks

--- My.hs
+++ My.hs
@@ -99,3 +99,7 @@ myZip (w:x) (y:z) = ((myTuple w y):(myZip x z))
 myUnzip :: [(a,b)] -> ([a], [b])
 myUnzip [] = ([],[])
 myUnzip ((w,x):b) = ((w:myFst(myUnzip(b))), (x:mySnd(myUnzip(b))))
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap function [] = []
+myMap function (a:b) = function a:myMap function b
