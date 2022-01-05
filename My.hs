e0bf06caf0a33114fe1fbea9694474a1b168371b
Mathieu Brujan <ken@localhost.localdomain>
2022-01-05 03:56:22 -0500

    First five tasks

--- My.hs
+++ My.hs
@@ -10,3 +10,13 @@ myAbs :: Int -> Int
 myAbs x
  | x < 0 = x * (-1)
  | otherwise = x
+
+myMin :: Int -> Int -> Int
+myMin x y
+ | x <= y = x
+ | otherwise = y
+
+myMax :: Int -> Int -> Int
+myMax x y
+ | x >= y = x
+ | otherwise = y
