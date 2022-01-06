35b13a025da27279bba6d2547648e4f089b9b4fd
Mathieu Brujan <ken@localhost.localdomain>
2022-01-06 03:30:15 -0500

    Task 02

--- DoOp.hs
+++ DoOp.hs
@@ -1,5 +1,11 @@
+import Data.Maybe
+
 myElem :: Eq a => a -> [a] -> Bool
 myElem argument [] = False
 myElem argument (element:list)
  | argument == element = True
  | argument /= element = myElem argument list
+
+safeDiv :: Int -> Int -> Maybe Int
+safeDiv _ 0 = Nothing
+safeDiv dividend divisor = Just (dividend `div` divisor)
