bcb47455433d8aed6552dfe10f60eeed385aaea2
Mathieu Brujan <ken@localhost.localdomain>
2022-01-06 04:12:14 -0500

    Task 07

--- DoOp.hs
+++ DoOp.hs
@@ -31,3 +31,30 @@ maybeDo :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
 maybeDo _ _ Nothing = Nothing
 maybeDo _ Nothing _ = Nothing
 maybeDo function (Just a) (Just b) = Just (function a b)
+
+
+myIsDigit :: Char -> Bool
+myIsDigit '0' = True
+myIsDigit '1' = True
+myIsDigit '2' = True
+myIsDigit '3' = True
+myIsDigit '4' = True
+myIsDigit '5' = True
+myIsDigit '6' = True
+myIsDigit '7' = True
+myIsDigit '8' = True
+myIsDigit '9' = True
+myIsDigit x = False
+
+myCheckNumber :: [Char] -> Bool
+myCheckNumber "" = True
+myCheckNumber (chara:string)
+ | myIsDigit chara == True = myCheckNumber(string)
+ | otherwise = False
+
+readInt :: [Char] -> Maybe Int
+readInt "" = Nothing
+readInt string
+ | myCheckNumber string == False = Nothing
+ | otherwise = Just (read string :: Int)
+ 
