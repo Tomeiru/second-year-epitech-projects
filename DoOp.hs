f778908dd9e84fb7bf541fff686b468b1534312e
Mathieu Brujan <ken@localhost.localdomain>
2022-01-06 03:43:25 -0500

    Task 04

--- DoOp.hs
+++ DoOp.hs
@@ -9,3 +9,14 @@ myElem argument (element:list)
 safeDiv :: Int -> Int -> Maybe Int
 safeDiv _ 0 = Nothing
 safeDiv dividend divisor = Just (dividend `div` divisor)
+
+safeNth :: [a] -> Int -> Maybe a
+safeNth [] z = Nothing
+safeNth (x:y) 0 = Just x
+safeNth (x:y) z
+ | z < 0 = Nothing
+ | z > 0 = safeNth y (z - 1)
+
+safeSucc :: Maybe Int -> Maybe Int
+safeSucc Nothing = Nothing
+safeSucc (Just x) = Just (x + 1)
