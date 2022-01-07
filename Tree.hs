db5b8e0b23c1d4090a784cf1d09c8522bb75cc77
Mathieu Brujan <ken@localhost.localdomain>
2022-01-07 06:45:33 -0500

    Fix Task 1é

--- Tree.hs
+++ Tree.hs
@@ -12,5 +12,5 @@ instance Functor Tree where
 addInTree :: Ord a => a -> Tree a -> Tree a
 addInTree value Empty = Node Empty value Empty
 addInTree value (Node tree y tree')
- | value < y = (Node (Node Empty value Empty) y tree')
- | otherwise = (Node tree y (Node Empty value Empty))
+ | value < y = (Node (addInTree value tree) y tree')
+ | otherwise = (Node tree y (addInTree value tree'))
