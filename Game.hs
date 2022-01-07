30db408e8698511f83c61d7056a5d113692a426a
Mathieu Brujan <ken@localhost.localdomain>
2022-01-07 03:59:35 -0500

    First 05 Tasks

--- Game.hs
+++ Game.hs
@@ -1,5 +1,9 @@
+-- Task 01 and 02
+
 data Item = Sword | Bow | MagicWand
     deriving (Eq)
+
+-- Task 03
 
 instance Show Item where
     show = showItem
@@ -8,3 +12,25 @@ showItem :: Item -> String
 showItem Sword = "sword"
 showItem Bow = "bow"
 showItem MagicWand = "magic wand"
+
+-- Task 04
+
+data Mob a = Mummy | Skeleton Item | Witch (Maybe Item)
+    deriving (Eq)
+
+-- Task 05
+
+createMummy :: Mob a
+createMummy = Mummy
+
+createArcher :: Mob a
+createArcher = Skeleton Bow
+
+createKnight :: Mob a
+createKnight = Skeleton Sword
+
+createWitch :: Mob a
+createWitch = Witch Nothing
+
+createSorceress :: Mob a
+createSorceress = Witch (Just MagicWand)
