cb10d40e3b68d29844d15e261ab02d6550ede836
Mathieu Brujan <ken@localhost.localdomain>
2022-01-07 05:57:23 -0500

    Task 12

--- Tree.hs
+++ Tree.hs
@@ -1,4 +1,12 @@
--- Task 01
+-- Task 11
 
 data Tree a = Empty | Node (Tree a) a (Tree a) 
     deriving (Show)
+
+-- Task 12
+
+addInTree :: Ord a => a -> Tree a -> Tree a
+addInTree value Empty = Node Empty value Empty
+addInTree value (Node tree y tree')
+ | value < y = (Node (Node Empty value Empty) y tree')
+ | otherwise = (Node tree y (Node Empty value Empty))
