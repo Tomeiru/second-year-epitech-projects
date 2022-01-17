978be24768c02f96923b0c7819006c58fb620591
Mathieu Brujan <ken@desktop-j3r1j88.home>
2022-01-17 03:55:11 -0500

    Adding Task00

---
+++ ex00/include/z.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** AM
+** File description:
+** z
+*/
+
+#ifndef Z_H_
+#define Z_H_
+
+#include <unistd.h>
+
+#endif /* !Z_H_ */
---
+++ ex00/z.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** AM
+** File description:
+** z
+*/
+
+#include "z.h"
+
+int main(void)
+{
+    write(1, "z\n", 2);
+    return (0);
+}
