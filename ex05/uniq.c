c347b9266ebcc3d74a09599235f7c9d0bf3f839f
Mathieu Brujan <ken@desktop-j3r1j88.home>
2022-01-17 12:07:02 -0500

    Renorming Task05

--- ex05/uniq.c
+++ ex05/uniq.c
@@ -8,18 +8,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+int *uniq_int_array_scd(int *array, size_t *nmenb, int i, int *j)
+{
+    if (array[i] == array[*j]) {
+        for (int k = *j + 1; k < *nmenb; k++)
+            array[k - 1] = array[k];
+        *nmenb = *nmenb - 1;
+        *j = *j - 1;
+    }
+    return (array);
+}
+
 size_t uniq_int_array(int *array, size_t nmenb)
 {
-    for (int i = 0; i < nmenb; i++) {
-        for (int j = i + 1; j < nmenb; j++) {
-            if (array[i] == array[j]) {
-                for (int k = j + 1; k < nmenb; k++)
-                    array[k - 1] = array[k];
-                nmenb--;
-                j--;
-            }
-        }
-    }return (nmenb);
+    for (int i = 0; i < nmenb; i++)
+        for (int j = i + 1; j < nmenb; j++)
+            array = uniq_int_array_scd(array, &nmenb, i, &j);
+    return (nmenb);
 }
 
 size_t uniq_array(void *array, size_t nmenb, size_t size, int (*compar)
