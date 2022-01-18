b4aaf95e25ed70c0dbaabc0e41d131c2c0acddec
Mathieu Brujan <ken@desktop-j3r1j88.home>
2022-01-18 07:01:22 -0500

    Exercise 07?

---
+++ string_copy.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2022
+** Day5
+** File description:
+** string_copy
+*/
+
+#include "string.h"
+
+static size_t det_len_s(const char *str, size_t n, size_t pos)
+{
+    int i = 0;
+
+    for (; str[pos + i] != '\0' && i < n; i++);
+    return (i);
+}
+
+size_t copy(const string_t *this, char *s, size_t n, size_t pos)
+{
+    size_t ret = 0;
+
+    if (this == NULL)
+        return (0);
+    ret = det_len_s(this->str, n, pos);
+    strncpy(s, this->str + pos, n);
+    return (ret);
+}
