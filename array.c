e9a4c3bd825ab7029c84f985c9b24ebd99bfd8bf
Mathieu Brujan <ken@pc-61.home>
2022-01-19 12:05:25 -0500

    Reforming the array.c file according to the Epitech C Coding Style

--- array.c
+++ array.c
@@ -11,8 +11,7 @@
 #include "array.h"
 #include "new.h"
 
-typedef struct
-{
+typedef struct {
     Container base;
     Class *_type;
     size_t _size;
@@ -25,40 +24,43 @@ typedef struct {
     size_t _idx;
 } ArrayIteratorClass;
 
-static void ArrayIterator_ctor(ArrayIteratorClass *this, va_list *args)
+static void array_iterator_ctor(ArrayIteratorClass *this, va_list *args)
 {
     this->_array = va_arg(*args, ArrayClass *);
     this->_idx = va_arg(*args, int);
 }
 
-static bool ArrayIterator_eq(ArrayIteratorClass *this, ArrayIteratorClass *other)
+static bool array_iterator_eq(ArrayIteratorClass *this,
+ArrayIteratorClass *other)
 {
     return (this->_idx == other->_idx);
 }
 
-static bool ArrayIterator_gt(ArrayIteratorClass *this, ArrayIteratorClass *other)
+static bool array_iterator_gt(ArrayIteratorClass *this,
+ArrayIteratorClass *other)
 {
     return (this->_idx > other->_idx);
 }
 
-static bool ArrayIterator_lt(ArrayIteratorClass *this, ArrayIteratorClass *other)
+static bool array_iterator_lt(ArrayIteratorClass *this,
+ArrayIteratorClass *other)
 {
     return (this->_idx < other->_idx);
 }
 
-static void ArrayIterator_incr(ArrayIteratorClass *this)
+static void array_iterator_incr(ArrayIteratorClass *this)
 {
     this->_idx += 1;
 }
 
-static Object *ArrayIterator_getval(ArrayIteratorClass *this)
+static Object *array_iterator_getval(ArrayIteratorClass *this)
 {
     if (this->_idx >= this->_array->_size)
         raise("Out of range");
     return (this->_array->_tab[this->_idx]);
 }
 
-static void ArrayIterator_setval(ArrayIteratorClass *this, ...)
+static void array_iterator_setval(ArrayIteratorClass *this, ...)
 {
     va_list args;
 
@@ -71,24 +73,24 @@ static void ArrayIterator_setval(ArrayIteratorClass *this, ...)
 }
 
 static const ArrayIteratorClass ArrayIteratorDescr = {
-    {   /* Iterator struct */
-        {   /* Class struct */
+    {
+        {
             .__size__ = sizeof(ArrayIteratorClass),
             .__name__ = "ArrayIterator",
-            .__ctor__ = (ctor_t)&ArrayIterator_ctor,
+            .__ctor__ = (ctor_t)&array_iterator_ctor,
             .__dtor__ = NULL,
             .__str__ = NULL,
             .__add__ = NULL,
             .__sub__ = NULL,
             .__mul__ = NULL,
             .__div__ = NULL,
-            .__eq__ = (binary_comparator_t)&ArrayIterator_eq,
-            .__gt__ = (binary_comparator_t)&ArrayIterator_gt,
-            .__lt__ = (binary_comparator_t)&ArrayIterator_lt,
+            .__eq__ = (binary_comparator_t)&array_iterator_eq,
+            .__gt__ = (binary_comparator_t)&array_iterator_gt,
+            .__lt__ = (binary_comparator_t)&array_iterator_lt,
         },
-        .__incr__ = (incr_t)&ArrayIterator_incr,
-        .__getval__ = (getval_t)&ArrayIterator_getval,
-        .__setval__ = (setval_t)&ArrayIterator_setval,
+        .__incr__ = (incr_t)&array_iterator_incr,
+        .__getval__ = (getval_t)&array_iterator_getval,
+        .__setval__ = (setval_t)&array_iterator_setval,
     },
     ._array = NULL,
     ._idx = 0
@@ -96,7 +98,7 @@ static const ArrayIteratorClass ArrayIteratorDescr = {
 
 static const Class *ArrayIterator = (const Class *)&ArrayIteratorDescr;
 
-static void Array_ctor(ArrayClass *this, va_list *args)
+static void array_ctor(ArrayClass *this, va_list *args)
 {
     va_list *temp = malloc(sizeof(va_list));
 
@@ -111,29 +113,29 @@ static void Array_ctor(ArrayClass *this, va_list *args)
     free(temp);
 }
 
-static void Array_dtor(ArrayClass *this)
+static void array_dtor(ArrayClass *this)
 {
     for (unsigned int i = 0; i < this->_size; i++)
         delete(this->_tab[i]);
     free(this->_tab);
 }
 
-static size_t Array_len(ArrayClass *this)
+static size_t array_len(ArrayClass *this)
 {
     return (this->_size);
 }
 
-static Iterator *Array_begin(ArrayClass *this)
+static Iterator *array_begin(ArrayClass *this)
 {
     return (new(ArrayIterator, this, 0));
 }
 
-static Iterator *Array_end(ArrayClass *this)
+static Iterator *array_end(ArrayClass *this)
 {
     return (new(ArrayIterator, this, this->_size));
 }
 
-static Object *Array_getitem(ArrayClass *this, ...)
+static Object *array_getitem(ArrayClass *this, ...)
 {
     va_list args;
     size_t index;
@@ -146,7 +148,7 @@ static Object *Array_getitem(ArrayClass *this, ...)
     return (this->_tab[index]);
 }
 
-static void Array_setitem(ArrayClass *this, ...)
+static void array_setitem(ArrayClass *this, ...)
 {
     va_list args;
     size_t index;
@@ -163,12 +165,12 @@ static void Array_setitem(ArrayClass *this, ...)
 }
 
 static const ArrayClass _descr = {
-    {   /* Container struct */
-        {   /* Class struct */
+    {
+        {
             .__size__ = sizeof(ArrayClass),
             .__name__ = "Array",
-            .__ctor__ = (ctor_t)&Array_ctor,
-            .__dtor__ = (dtor_t)&Array_dtor,
+            .__ctor__ = (ctor_t)&array_ctor,
+            .__dtor__ = (dtor_t)&array_dtor,
             .__str__ = NULL,
             .__add__ = NULL,
             .__sub__ = NULL,
@@ -178,15 +180,15 @@ static const ArrayClass _descr = {
             .__gt__ = NULL,
             .__lt__ = NULL,
         },
-        .__len__ = (len_t)&Array_len,
-        .__begin__ = (iter_t)&Array_begin,
-        .__end__ = (iter_t)&Array_end,
-        .__getitem__ = (getitem_t)&Array_getitem,
-        .__setitem__ = (setitem_t)&Array_setitem,
+        .__len__ = (len_t)&array_len,
+        .__begin__ = (iter_t)&array_begin,
+        .__end__ = (iter_t)&array_end,
+        .__getitem__ = (getitem_t)&array_getitem,
+        .__setitem__ = (setitem_t)&array_setitem,
     },
     ._type = NULL,
     ._size = 0,
     ._tab = NULL
 };
 
-const Class   *Array = (const Class *)&_descr;
+const Class *Array = (const Class *)&_descr;
