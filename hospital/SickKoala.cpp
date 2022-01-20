b486190cdd4231caf5c35279f26eda2a57530369
Mathieu Brujan <ken@localhost.localdomain>
2022-01-20 06:31:35 -0500

    Exercise 02

---
+++ hospital/SickKoala.cpp
@@ -0,0 +1,44 @@
+/*
+** EPITECH PROJECT, 2022
+** Day6
+** File description:
+** SickKoala
+*/
+
+#include "SickKoala.hpp"
+
+SickKoala::SickKoala(std::string new_name)
+{
+    name = new_name;
+}
+
+SickKoala::~SickKoala()
+{
+    std::cout << "Mr." << name << ": Kreooog!! I'm cuuuured!" << std::endl;
+}
+
+void SickKoala::poke()
+{
+    std::cout << "Mr." << name << ": Gooeeeeerrk" << std::endl;
+}
+
+bool SickKoala::takeDrug(std::string drug)
+{
+    if (drug == "Mars") {
+        std::cout << "Mr." << name << ": Mars, and it kreogs!" << std::endl;
+        return (true);
+    }if (drug == "Kinder") {
+        std::cout << "Mr." << name << ": There is a toy inside!" << std::endl;
+        return (true);
+    }std::cout << "Mr." << name << ": Goerkreog" << std::endl;
+    return (false);
+}
+
+void SickKoala::overDrive(std::string sentence)
+{
+    std::string kreog = "Kreog!";
+
+    while (sentence.find(kreog) != std::string::npos)
+        sentence.replace(sentence.find(kreog), kreog.length(), "1337");
+    std::cout << "Mr." << name << ": " << sentence << std::endl;
+}
---
+++ hospital/SickKoala.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2022
+** Day6
+** File description:
+** SickKoala
+*/
+
+#ifndef SICKKOALA_HPP_
+#define SICKKOALA_HPP_
+
+#include <iostream>
+
+class SickKoala {
+    public:
+        SickKoala(std::string);
+        ~SickKoala();
+        void poke();
+        bool takeDrug(std::string);
+        void overDrive(std::string);
+
+    std::string name;
+};
+
+#endif /* !SICKKOALA_HPP_ */
