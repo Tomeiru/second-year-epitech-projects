623992c33ba9c561b96e5c7d5f2c7bcf4bbcf33d
Mathieu Brujan <ken@localhost.localdomain>
2022-01-20 05:49:06 -0500

    Exercise 01

---
+++ ex01/my_convert_temp.cpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2022
+** Day6
+** File description:
+** my_convert_temp
+*/
+
+#include <iostream>
+#include <iomanip>
+
+int main(void)
+{
+    double temp;
+    std::string unit;
+    std::cin >> temp;
+    std::cin >> unit;
+
+    if (unit == "Fahrenheit") {
+        temp = 5.0 / 9.0 * (temp - 32);
+        unit = " Celsius";
+    }
+    if (unit == "Celsius") {
+        temp = temp / (5.0 / 9.0) + 32;
+        unit = " Fahrenheit";
+    }
+    std::cout << std::setw(16) << std::fixed <<
+    std::setprecision(3) << temp << std::setw(16) << unit << std::endl;
+}
