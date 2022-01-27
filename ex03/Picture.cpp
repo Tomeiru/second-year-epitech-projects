a201a2676eae23865d651b3b97dd6a6f12820eb7
Mathieu Brujan <ken@localhost.localdomain>
2022-01-27 05:04:57 -0500

    Exercise 03

---
+++ ex03/Picture.cpp
@@ -0,0 +1,54 @@
+/*
+** EPITECH PROJECT, 2022
+** Day11
+** File description:
+** Picture
+*/
+
+#include "Picture.hpp"
+
+Picture::Picture(const std::string &file)
+{
+    std::ifstream content(file);
+    std::stringstream contentStream;
+
+    if (content.is_open()) {
+        contentStream << content.rdbuf();
+        _data = contentStream.str();
+        return;
+    }
+    _data = "ERROR";
+}
+
+Picture::Picture(const Picture &other)
+{
+    _data = other._data;
+}
+
+Picture::Picture()
+{
+    _data = "";
+}
+
+Picture::~Picture()
+{
+}
+
+bool Picture::getPictureFromFile(const std::string &file)
+{
+    std::ifstream content(file);
+    std::stringstream contentStream;
+
+    if (content.is_open()) {
+        contentStream << content.rdbuf();
+        _data = contentStream.str();
+        return (true);
+    }
+    _data = "ERROR";
+    return (false);
+}
+
+void Picture::operator=(const Picture &other)
+{
+    _data = other._data;
+}
---
+++ ex03/Toy.hpp
@@ -0,0 +1,40 @@
+/*
+** EPITECH PROJECT, 2022
+** Day11
+** File description:
+** Toy
+*/
+
+#ifndef TOY_HPP_
+#define TOY_HPP_
+
+#include "Picture.hpp"
+
+class Toy {
+    public:
+        enum ToyType {
+            BASIC_TOY,
+            ALIEN,
+            BUZZ,
+            WOODY
+        };
+        Toy(const ToyType &, const std::string &, const std::string &);
+        Toy(const Toy &);
+        Toy();
+        ~Toy();
+        ToyType getType() const;
+        std::string getName() const;
+        std::string getAscii() const;
+        void setAscii(const std::string &);
+        void setName(const std::string &);
+        void operator=(const Toy &);
+        virtual void speak(const std::string &);
+    protected:
+        ToyType _type;
+        std::string _name;
+        Picture _ascii;
+    private:
+
+};
+
+#endif /* !TOY_HPP_ */
