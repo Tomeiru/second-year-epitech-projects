7987e64e6e2332f3ce88f30005b5e1abaf4894cf
Mathieu Brujan <ken@localhost.localdomain>
2022-01-31 12:26:20 -0500

    Changement in the display of the ncurses basics for it to be centered

--- src/nCurses.cpp
+++ src/nCurses.cpp
@@ -10,16 +10,22 @@
 
 int startCurses(MyGKrellmInfo *info)
 {
+    int windowWidth = 0;
+    int windowHeight = 0;
+
+
     initscr();
+    getmaxyx(stdscr, windowHeight, windowWidth);
     curs_set(0);
     while (1) {
-        mvprintw(0, 0, "%s\n", info->getHostname()->getData().c_str());
-        mvprintw(1, 0, "%s\n", info->getUsername()->getData().c_str());
-        mvprintw(2, 0, "%s %s\n", info->getOS()->getData().c_str(), info->getKernelVersion()->getData().c_str());
-        mvprintw(3, 0, "%s\n", info->getDateTime()->getData().c_str());
+        mvprintw(windowHeight / 2 - 2, windowWidth / 2 - info->getHostname()->getData().length() / 2, "%s\n", info->getHostname()->getData().c_str());
+        mvprintw(windowHeight / 2 - 1, windowWidth / 2 - info->getUsername()->getData().length() / 2, "%s\n", info->getUsername()->getData().c_str());
+        mvprintw(windowHeight / 2, windowWidth / 2 - info->getOS()->getData().length() / 2 - info->getKernelVersion()->getData().length() / 2, "%s %s\n", info->getOS()->getData().c_str(), info->getKernelVersion()->getData().c_str());
+        mvprintw(windowHeight / 2 + 1, windowWidth / 2 - info->getDateTime()->getData().length() / 2, "%s\n", info->getDateTime()->getData().c_str());
         info->getDateTime()->updateData();
         refresh();
     }
     endwin();
+    std::cout << windowHeight << " " << windowWidth << std::endl;
     return (0);
 }
