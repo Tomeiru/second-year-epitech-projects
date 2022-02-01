d827f97d6a6ff1e4a4f80046ff06a6a22bacab4e
Mathieu Brujan <ken@desktop-j3r1j88.home>
2022-02-01 06:49:41 -0500

    Changes to the nCurses function git push

--- src/nCurses.cpp
+++ src/nCurses.cpp
@@ -34,6 +34,7 @@ void inputGestion(MyGKrellmInfo *info)
 
 void resizeHandler(int dummy)
 {
+    (void*)(&dummy);
     clear();
 }
 
@@ -50,19 +51,23 @@ int startCurses(MyGKrellmInfo *info)
     cbreak();
     refresh();
     curs_set(0);
-    box(my_win, 0, 0);
     while (1) {
         for (ModuleList_t *temp = info->getModules(); temp != nullptr; temp = temp->next)
-            if (temp->module->getDisplayed() == true) {
+            if (info->getHelpModule()->getDisplayed() == true)
+                mvprintw(0, 0, "%s", info->getHelpModule()->getData().c_str());
+            else if (temp->module->getDisplayed() == true) {
+                box(my_win, 0, 0);
                 mvwprintw(my_win, a++, width / 2 - temp->module->getTitle().length() / 2, "%s", temp->module->getTitle().c_str());
                 mvwprintw(my_win, a++, width / 2 - temp->module->getData().length() / 2, "%s", temp->module->getData().c_str());
                 a += 2;
             }
         info->getDateTime()->updateData();
+        timeout(0);
+        inputGestion(info);
+        signal(SIGWINCH, resizeHandler);
         wrefresh(my_win);
         a = 2;
     }
     endwin();
-    std::cout << height << " " << width << std::endl;
     return (0);
 }
