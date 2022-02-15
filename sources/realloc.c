65a24d74918a7325d7d7ce7aa3ce9ea93af00f6c
Mathieu Brujan <ken@localhost.localdomain>
2022-02-15 06:22:10 -0500

    First realloc version

--- sources/realloc.c
+++ sources/realloc.c
@@ -9,7 +9,22 @@
 
 void *realloc(void *ptr, size_t size)
 {
-    UNUSED(ptr);
-    UNUSED(size);
-    return (NULL);
+    void *ret;
+    metadata_t *old_data;
+    metadata_t *new_data;
+
+    if (ptr == NULL)
+        return (malloc(size));
+    if (size == 0) {
+        free(ptr);
+        return (NULL);
+    }
+    free(ptr);
+    ret = malloc(size);
+    if (ret == NULL)
+        return (NULL);
+    old_data = ptr - sizeof(metadata_t);
+    new_data = ret - sizeof(metadata_t);
+    memcpy(ret, ptr, (old_data->size >= new_data->size) ? (new_data->size) : (old_data->size));
+    return (ret);
 }
