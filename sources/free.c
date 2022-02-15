1d13537282f32dc3bb42e03caa002402863a8667
Mathieu Brujan <ken@localhost.localdomain>
2022-02-15 05:54:41 -0500

    Removing opimization for the free function

--- sources/free.c
+++ sources/free.c
@@ -17,17 +17,7 @@ void free(void *ptr)
     if (metadata->magic[0] != -115) {
         write(2, "free(): invalid pointer\n", 24);
         return;
-    }metadata->free = 1;
-    if (metadata->next != NULL && metadata->next->free == 1) {
-        metadata->size = metadata->next->size + sizeof(metadata_t) + metadata->size;
-        metadata->next = metadata->next->next;
-        if (metadata->next->next != NULL)
-            metadata->next->next->prev = metadata;
-    }if (metadata->prev != NULL && metadata->prev->free == 1) {
-        metadata->prev->size = metadata->prev->size + sizeof(metadata_t) + metadata->size;
-        metadata->prev->next = metadata->next;
-        if (metadata->next != NULL)
-            metadata->next->prev = metadata->prev;
-        metadata = metadata->prev;
-    }return;
+    }
+    metadata->free = 1;
+    return;
 }
