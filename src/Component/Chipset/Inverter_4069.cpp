f15770c8f9d79fd7cecf91a4ccedaf2817467498
Mathieu Brujan <ken@localhost.localdomain>
2022-03-04 16:45:12 -0500

    Correction to inverter

--- src/Component/Chipset/Inverter_4069.cpp
+++ src/Component/Chipset/Inverter_4069.cpp
@@ -23,7 +23,7 @@ void Inverter_4069::simulate(std::size_t tick)
     setSinglePin(1, gate.not_gate(_Pin[0]));
     setSinglePin(3, gate.not_gate(_Pin[2]));
     setSinglePin(5, gate.not_gate(_Pin[4]));
-    setSinglePin(8, gate.not_gate(_Pin[7]));
-    setSinglePin(10, gate.not_gate(_Pin[9]));
-    setSinglePin(12, gate.not_gate(_Pin[11]));
+    setSinglePin(7, gate.not_gate(_Pin[8]));
+    setSinglePin(9, gate.not_gate(_Pin[10]));
+    setSinglePin(11, gate.not_gate(_Pin[12]));
 }
