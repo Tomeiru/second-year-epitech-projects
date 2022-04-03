3b54c758a28c6fb92aaf0005cade5bc436c38c48
Gabriel Ravier <gabravier@gmail.com>
2022-04-03 18:35:25 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** strace
+** File description:
+** Defines main
+*/
+
+int main(int argc, char **argv)
+{
+    /*
+    static struct strace self;
+
+    strace_init(&self, argc, argv);*/
+}
